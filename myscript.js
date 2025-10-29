65b58074b25f51edab68811f3f6703214c98d802
neoXOT <neofillon0x@gmail.com>
2025-10-29 14:10:53 +0100

    Add wallet connection alert on button click

---
+++ myscript.js
@@ -0,0 +1,8 @@
+// Get the button element
+const connectButton = document.getElementById('connectButton');
+
+// Add a click event listener
+connectButton.addEventListener('click', () => {
+    // When clicked, show a simple alert
+    alert('Wallet connection feature coming soon!');
+});
